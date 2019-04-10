5c4a35f1d959e95d70f218ae17cca0a0137a1544
Thankam Girija <thankam@gmail.com>
2019-04-10 09:45:16 -0400

    change detection timeout changes

--- my-angular-albums/src/app/app.component.ts
+++ my-angular-albums/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { Album } from "./albums/album.model";
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,9 +11,32 @@ export class AppComponent implements OnInit {
 
   title = 'My Angular Albums';
   albumsArray: Album[];
+  titleCounter = 1;
+  numbers: number[];
 
 
   ngOnInit(): void {
+    // change detection
+    // create an interval timer, where every 2 seconds,
+    // you update the titleCounter by one. After 6 seconds cancel the interval.
+    const interval = setInterval(() => this.titleCounter++, 2000);
+    setTimeout(() => clearInterval(interval), 6000);
+
+
+    this.numbers = [1, 2, 3];
+
+    const timerID = setInterval(() => {
+      // this.numbers.push(4);
+      this.numbers = [...this.numbers, 4];
+      this.albumsArray[0].price += 10;
+      console.log(this.numbers);
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(timerID );
+    },  4000);
+
+
     this.albumsArray = [
       {
         id: 1,
